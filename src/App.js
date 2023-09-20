8dc8b424824a0e41a4a13656af63a2cd2000d106
medhavi0407 <medhavisaxena0407@gmail.com>
2023-09-21 01:20:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import CreateAndSell from './components/CreateAndSell';
 import Choose from './components/Choose';
 import Marketplace from './components/Marketplace';
 import Subscribe from './components/Subscribe';
+import Footer from "./components/Footer";
 
 export default function App() {
   return( 
     <section>
+      
       <Home/>
       <Clients/>
       <CreateAndSell/>
       <Choose/>
       <Marketplace/>
       <Subscribe/>
+      <Footer/>
     </section>
   )
 }
